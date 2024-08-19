780482471b58b6c1052afc55977be90fe47f1c13
David Ramírez <rrdavid08@gmail.com>
2024-08-20 01:48:50 +0200

    feat: Update Square component styling and add index parameter to updateBoard function

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,12 @@
-export default function Square({children, updateBoard, isSelected, style}) {
-  const className = isSelected ? 'bg-blue-600' : ''
+export default function Square({children, index, updateBoard, isSelected, style}) {
+  const className = isSelected ? 'bg-blue-600 hover:bg-blue-600' : ''
 
   function handleClick() {
-    updateBoard()
+    updateBoard(index)
   }
   return (
     <div
-      className={`w-full h-full flex justify-center items-center text-8xl border-2 border-gray-200 hover:bg-gray-700 hover:cursor-pointer rounded-none hover:border-gray-200 ${className} ${style}`}
+      className={`aspect-square flex justify-center items-center text-6xl border-2 border-gray-200 hover:bg-gray-700 hover:cursor-pointer rounded-none hover:border-gray-200 ${style} ${className}`}
       onClick={handleClick}
     >{children}</div>
   )
