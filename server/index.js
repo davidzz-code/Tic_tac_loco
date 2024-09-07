f036588892a210cb004be18bb60ce17e1c78e920
David Ramírez <rrdavid08@gmail.com>
2024-09-07 17:58:47 +0200

    refactor: Debugging

--- server/index.js
+++ server/index.js
@@ -7,55 +7,18 @@ const server = createServer()
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: 'http://localhost:5173',
   },
-  connectionStateRecovery: {
-    
-  }
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
 
   socket.on('syncBoard', (data) => {
-    io.emit('syncBoard', data)
+    console.log('Received syncBoard with data:', data.turn);
+    socket.broadcast.emit('syncBoard', data)
   })
 })
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
-
-
-
-
-// import express from 'express';
-// import logger from 'morgan';
-
-// import { Server } from 'socket.io';
-// import { createServer } from 'node:http';
-
-// const port = process.env.PORT ?? 3000
-
-// const app = express()
-// const server = createServer(app)
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000", // Cambia esto al origen de tu frontend
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected')
-// })
-
-// app.use(logger('dev'))
-
-// app.get('/', (req, res) => { 
-//   res.sendFile(process.cwd() + '/client/index.html')
-// })
-
-// server.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// })
