d76eb2e3b74f7ae8e96bcaaf2b38a56031f87fc2
davidzz-code <rrdavid08@gmail.com>
2024-10-26 16:13:59 +0200

    Refactor: Update GameMode component UI and styles

--- client/src/components/GameMode.jsx
+++ client/src/components/GameMode.jsx
@@ -18,12 +18,24 @@ export default function GameMode({ setGameMode, setIsGameModeSelected }) {
   }
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-x-5">
-      <h2 className="text-4xl">Tic tac Loco</h2>
-      <div className="mt-20">
-        <button onClick={handleClickSinglePlayer}>{ GAME_MODES.SINGLE }</button>
-        <button onClick={handleClickDoublePlayer}>{ GAME_MODES.DOUBLE }</button>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-[#242424] text-[#F0F0F0] relative">
+      <h2 className="text-7xl font-bold tracking-widest mb-12 text-[#CA8A03] z-10">
+        Tic Tac Loco
+      </h2>
+      <div className="flex flex-col space-y-8 z-10">
+        <button 
+          onClick={handleClickSinglePlayer} 
+          className="flex items-center justify-center px-10 py-3 text-lg rounded-md bg-transparent text-[#F0F0F0] border-2 border-white hover:border-[#3c82f6] transition-all duration-200"
+        >
+          {GAME_MODES.SINGLE}
+        </button>
+        <button 
+          onClick={handleClickDoublePlayer} 
+          className="flex items-center justify-center px-10 py-3 text-lg rounded-md bg-transparent text-[#F0F0F0] border-2 border-white hover:border-[#ef4444] transition-all duration-200"
+        >
+          {GAME_MODES.DOUBLE}
+        </button>
       </div>
     </div>
-  )
+  );
 }
